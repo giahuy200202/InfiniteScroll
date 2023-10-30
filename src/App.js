fd4def80f1e9a80ec62a8651d2abbd0553bcff33
GiaHuy <giahuy200202@gmail.com>
2023-10-30 21:02:25 +0700

    handle loading indication  when data is not ready

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ const App = () => {
   const accessKey = "fZPvD5cHD-hbV_rtKL2VhKs-DR9BJRki1PhgenaDl0U";
   const [img, setImg] = useState("");
   const [res, setRes] = useState([]);
+  const [isSearchDone, setIsSearchDone] = useState(false);
+  const [dataSubmit, setDataSubmit] = useState("");
 
   const submit = () => {
     fetchRequest();
-    setImg("");
+    setDataSubmit(img);
   };
 
   const fetchRequest = async () => {
+    setIsSearchDone(false);
     const data = await fetch(
-      `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${accessKey}&per_page=20`
+      `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${accessKey}&per_page=10000`
     );
     const dataJ = await data.json();
     const result = dataJ.results;
+    setIsSearchDone(true);
     console.log(result);
     setRes(result);
   };
@@ -25,33 +29,43 @@ const App = () => {
   return (
     <div className="container">
       <div className="nav">
-        <div>Infinite Scroll</div>
+        <div className="title">Infinite Scroll</div>
         <div className="search-container">
           <input
             className="input-container"
             type="text"
             placeholder="Search..."
             value={img}
-            onChange={(e) => setImg(e.target.value)}
-          />
+            onChange={(e) => {
+              setImg(e.target.value);
+            }}
+          />  
           <button className="button-container" type="submit" onClick={submit}>
             Search
           </button>
         </div>
       </div>
-      <div className="col-12 d-flex justify-content-evenly flex-wrap">
-        {res.map((val) => {
-          return (
-            <>
+      {isSearchDone ? (
+        <div className="img-containter">
+          {dataSubmit!=="" ? (
+            <div className="result-title">Results for "{dataSubmit}"</div>
+          ) : (
+            <div></div>
+          )}
+          {res.map((val, index) => {
+            return (
               <img
-                className="col-3 img-fluid img-thumbnail"
+                key={index}
+                className="each-img"
                 src={val.urls.small}
                 alt="val.alt_description"
               />
-            </>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="spinner"></div>
+      )}
     </div>
   );
 };
