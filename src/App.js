343ba8f36cb3b2d1a5958d9e4d9882dd04c99c47
GiaHuy <giahuy200202@gmail.com>
2023-11-02 20:25:57 +0700

    fix set state regarding res

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
-// import { useInfiniteQuery } from "react-query";
-// import InfiniteScroll from "react-infinite-scroll-component";
 
 const App = () => {
   const accessKey = "fZPvD5cHD-hbV_rtKL2VhKs-DR9BJRki1PhgenaDl0U";
@@ -11,19 +9,21 @@ const App = () => {
   const [page, setPage] = useState(1);
 
   const submit = () => {
-    fetchRequest();
+    fetchRequest(1);
     setDataSubmit(img);
   };
 
   const fetchRequest = async (page) => {
     setIsSearchDone(false);
     const data = await fetch(
-      `https://api.unsplash.com/search/photos?page=${page}&query=${img}&client_id=${accessKey}&per_page=15`
+      `https://api.unsplash.com/search/photos?page=${page}&query=${img}&client_id=${accessKey}&per_page=10`
     );
     const dataJ = await data.json();
     const result = dataJ.results;
     setIsSearchDone(true);
-    setRes([...res, result]);
+    console.log('res: ', res);
+    console.log('result: ', result);
+    page === 1 ? setRes(result) : setRes(current => [...current, ...result]);
   };
 
   useEffect(() => {
@@ -36,25 +36,19 @@ const App = () => {
 
   console.log(page);
 
-  const pageEnd = useRef();
   let num = 1;  
 
   const intersectionObserver = useRef();
 
   const lastRef = useCallback((element)=>{
     if (!isSearchDone) return;
-
     if (intersectionObserver.current) intersectionObserver.current.disconnect();
-
     intersectionObserver.current = new IntersectionObserver((el) => {
       if (el[0].isIntersecting) {
         loadMore();
-        console.log('trigger');
       }
     });
-    
     if (element) intersectionObserver.current.observe(element);
-
   }, [isSearchDone, loadMore]);
 
 
@@ -97,7 +91,7 @@ const App = () => {
               />
             );
           })}
-          <div className="spinner" ref={pageEnd}></div>
+          <div className="spinner"></div>
         </div>
         
       ) : (
