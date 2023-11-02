a962bd3c3a9dd92c594a1fcff316c3349408de27
GiaHuy <giahuy200202@gmail.com>
2023-11-02 18:57:57 +0700

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+// import { useInfiniteQuery } from "react-query";
+// import InfiniteScroll from "react-infinite-scroll-component";
+
 const App = () => {
   const accessKey = "fZPvD5cHD-hbV_rtKL2VhKs-DR9BJRki1PhgenaDl0U";
-  const [img, setImg] = useState("");
+  const [img, setImg] = useState("random");
   const [res, setRes] = useState([]);
   const [isSearchDone, setIsSearchDone] = useState(false);
-  const [dataSubmit, setDataSubmit] = useState("");
+  const [dataSubmit, setDataSubmit] = useState("random");
+  const [page, setPage] = useState(1);
 
   const submit = () => {
     fetchRequest();
     setDataSubmit(img);
   };
 
-  const fetchRequest = async () => {
+  const fetchRequest = async (page) => {
     setIsSearchDone(false);
     const data = await fetch(
-      `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${accessKey}&per_page=10000`
+      `https://api.unsplash.com/search/photos?page=${page}&query=${img}&client_id=${accessKey}&per_page=15`
     );
     const dataJ = await data.json();
     const result = dataJ.results;
     setIsSearchDone(true);
-    console.log(result);
-    setRes(result);
+    setRes([...res, result]);
   };
+
   useEffect(() => {
-    fetchRequest();
-  }, []);
+    fetchRequest(page);
+  }, [page]);
+
+  const loadMore = () =>{
+    setPage(prevPage => prevPage + 1)
+  }
+
+  console.log(page);
+
+  const pageEnd = useRef();
+  let num = 1;  
+
+  const intersectionObserver = useRef();
+
+  const lastRef = useCallback((element)=>{
+    if (!isSearchDone) return;
+
+    if (intersectionObserver.current) intersectionObserver.current.disconnect();
+
+    intersectionObserver.current = new IntersectionObserver((el) => {
+      if (el[0].isIntersecting) {
+        loadMore();
+        console.log('trigger');
+      }
+    });
+    
+    if (element) intersectionObserver.current.observe(element);
+
+  }, [isSearchDone, loadMore]);
+
 
   return (
     <div className="container">
@@ -39,7 +71,7 @@ const App = () => {
             onChange={(e) => {
               setImg(e.target.value);
             }}
-          />  
+          />
           <button className="button-container" type="submit" onClick={submit}>
             Search
           </button>
@@ -47,12 +79,15 @@ const App = () => {
       </div>
       {isSearchDone ? (
         <div className="img-containter">
-          {dataSubmit!=="" ? (
+          {dataSubmit !== "random" ? (
             <div className="result-title">Results for "{dataSubmit}"</div>
           ) : (
             <div></div>
           )}
           {res.map((val, index) => {
+            if (index + 1 == res.length) {
+              return <img key={index} ref={lastRef} className="each-img" src={val.urls.small} alt={val.alt_description} />
+            }
             return (
               <img
                 key={index}
@@ -62,7 +97,9 @@ const App = () => {
               />
             );
           })}
+          <div className="spinner" ref={pageEnd}></div>
         </div>
+        
       ) : (
         <div className="spinner"></div>
       )}
